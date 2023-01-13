a6c6d4219b174805aa05ff6f293eb0e0b73b7d6f
Guillaume Lambert <guillaume.lambert@orange.com>
2023-01-13 20:20:14 +0100

    [build] Migrate ifupdown2 version download to apt-get source
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>

--- scripts/check_rules_mk_files_versions.sh
+++ scripts/check_rules_mk_files_versions.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 cd ..
 rootdir=$(pwd)
-for pkgsrcname in isc-dhcp debootstrap bash kdump-tools lm-sensors snmpd iptables hiredis; do
+for pkgsrcname in isc-dhcp debootstrap bash kdump-tools lm-sensors snmpd iptables hiredis ifupdown2; do
     basevarname=$(echo $pkgsrcname"_VERSION" | tr [a-z\-] [A-Z_])
 
     cd $rootdir/src/$pkgsrcname/
